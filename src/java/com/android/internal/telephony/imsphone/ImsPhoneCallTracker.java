ea930c34a6aaaf5e402dd4e2766810a0098c247e
Francis Guevarra <fguevarra@cyngn.com>
2016-02-23 16:02:53 -0800

    Volte: set volte feature enable in ImsManager when Volte service is updated
    Change-Id: I5d9351a233a527d92d286927473d2ce8c7030ea7
    Issue-Id: CYNGNOS-2011

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -2273,6 +2273,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
                 }
                 mPhone.getServiceState().setRilImsRadioTechnology(getRilImsRadioTechnology());
                 mPhone.onFeatureCapabilityChanged();
+                mImsManager.setVolteCallCapability(isVolteEnabled());
 
                 mMetrics.writeOnImsCapabilities(
                         mPhone.getPhoneId(), mImsFeatureEnabled);
