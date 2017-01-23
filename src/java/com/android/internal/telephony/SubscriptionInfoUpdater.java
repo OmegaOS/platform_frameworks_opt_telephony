LineageOS/android_frameworks_opt_telephony

Subscription updater: don't turn data off if we've never set a deafult
In the case of an MSIM device that has only had 1 SIM and then you swap
the sim to another slot, the data will turn off, but it's the same SIM
and should not.

Change-Id: I77ed8c22663aa10d211e6f4de06b791c1a8f0971
Signed-off-by: Roman Birg <roman@cyngn.com>

src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -767,7 +767,11 @@ public class SubscriptionInfoUpdater extends Handler {
                             Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION,
                             SubscriptionManager.INVALID_SUBSCRIPTION_ID);
 
-                    if (realStoredDataSub != SubscriptionManager.getDefaultDataSubId()) {
+                    if (realStoredDataSub != SubscriptionManager.INVALID_SUBSCRIPTION_ID &&
+                            realStoredDataSub != SubscriptionManager.getDefaultDataSubId()) {
+                        logd("switching data off; real stored sub: " + realStoredDataSub
+                                + ", and we think the default sub id is now: "
+                                + SubscriptionManager.getDefaultDataSubId());
                         PhoneFactory.getDefaultPhone().setDataEnabled(false);
                     }
 

OmegaOS/platform_frameworks_opt_telephony

src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -722,7 +722,11 @@ public class SubscriptionInfoUpdater extends Handler {
                             Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION,
                             SubscriptionManager.INVALID_SUBSCRIPTION_ID);
 
-                    if (realStoredDataSub != SubscriptionManager.getDefaultDataSubscriptionId()) {
+                    if (realStoredDataSub != SubscriptionManager.INVALID_SUBSCRIPTION_ID &&
+                            realStoredDataSub != SubscriptionManager.getDefaultDataSubscriptionId()) {
+                        logd("switching data off; real stored sub: " + realStoredDataSub
+                                + ", and we think the default sub id is now: "
+                                + SubscriptionManager.getDefaultDataSubscriptionId());
                         PhoneFactory.getDefaultPhone().setDataEnabled(false);
                     }
                 } else if (insertedSimCount > 1) {